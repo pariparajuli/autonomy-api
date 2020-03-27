442989dde0a13ec7796364c319efafccc2045004
Linzy Hu <linzy.hu@gmail.com>
2020-03-27 18:09:02 +0800

    add const SymptomType

--- api/symptom.go
+++ api/symptom.go
@@ -4,24 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/bitmark-inc/autonomy-api/schema"
 )
 
-type symptom struct {
-	Name string `json:"name"`
-	Desc string `json:"desc"`
-}
-
-var symptoms = []symptom{
-	{"Fever", ""},
-	{"Cough", ""},
-	{"Fatigue", ""},
-	{"Difficulty breathing", ""},
-	{"Nasal congestion", ""},
-	{"Sore throat", ""},
-	{"Chest pain or pressure", ""},
-	{"Bluish lips or face", ""},
-}
-
 func (s *Server) getSymptoms(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"symptoms": symptoms})
+	c.JSON(http.StatusOK, gin.H{"symptoms": schema.Symptoms})
 }
