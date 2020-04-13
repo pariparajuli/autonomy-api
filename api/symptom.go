548070300c56ffb84058922d5786cbf8246c6e4f
pieceofr <komimi.p@gmail.com>
2020-04-13 14:16:36 +0800

    add weight field to symptom and convert schema symptom to response symptom

--- api/symptom.go
+++ api/symptom.go
@@ -9,5 +9,17 @@ import (
 )
 
 func (s *Server) getSymptoms(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"symptoms": schema.Symptoms})
+	resp := []SymptomResp{}
+	for _, symptom := range schema.Symptoms {
+		respSymptom := SymptomResp{ID: symptom.ID, Name: symptom.Name, Desc: symptom.Desc}
+		resp = append(resp, respSymptom)
+	}
+	c.JSON(http.StatusOK, gin.H{"symptoms": resp})
+}
+
+// SymptomResp  respons struct of a symptom
+type SymptomResp struct {
+	ID   schema.SymptomType `json:"id"`
+	Name string             `json:"name"`
+	Desc string             `json:"desc"`
 }
--- schema/symptom.go
+++ schema/symptom.go
@@ -14,18 +14,19 @@ const (
 )
 
 type Symptom struct {
-	ID   SymptomType `json:"id"`
-	Name string      `json:"name"`
-	Desc string      `json:"desc"`
+	ID     SymptomType `json:"id"`
+	Name   string      `json:"name"`
+	Desc   string      `json:"desc"`
+	Weight float64     `json:"weight"`
 }
 
 var Symptoms = []Symptom{
-	{SymptomFever, "Fever", "Body temperature above 100ºF (38ºC)"},
-	{SymptomCough, "Dry cough", "Without mucous or phlegm (rattling)"},
-	{SymptomFatigue, "Fatigue or tiredness", "Unusual lack of energy or feeling run down"},
-	{SymptomBreath, "Shortness of breath", "Constriction or difficulty inhaling fully"},
-	{SymptomNasal, "Nasal congestion", "Stuffy or blocked nose"},
-	{SymptomThroat, "Sore throat", "Throat pain, scratchiness, or irritation"},
-	{SymptomChest, "Chest pain", "Persistent pain or pressure in the chest"},
-	{SymptomFace, "Bluish lips or face", "Not caused by cold exposure"},
+	{SymptomFever, "Fever", "Body temperature above 100ºF (38ºC)", 2},
+	{SymptomCough, "Dry cough", "Without mucous or phlegm (rattling)", 1},
+	{SymptomFatigue, "Fatigue or tiredness", "Unusual lack of energy or feeling run down", 1},
+	{SymptomBreath, "Shortness of breath", "Constriction or difficulty inhaling fully", 1},
+	{SymptomNasal, "Nasal congestion", "Stuffy or blocked nose", 1},
+	{SymptomThroat, "Sore throat", "Throat pain, scratchiness, or irritation", 1},
+	{SymptomChest, "Chest pain", "Persistent pain or pressure in the chest", 1},
+	{SymptomFace, "Bluish lips or face", "Not caused by cold exposure", 1},
 }
