bf8ad65620f25abbd693e56db812e94b4d76c1b1
pieceofr <komimi.p@gmail.com>
2020-04-20 22:44:49 +0800

    correct score caculation

--- api/symptom.go
+++ api/symptom.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"context"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/bitmark-inc/autonomy-api/consts"
 	"github.com/bitmark-inc/autonomy-api/schema"
+	"github.com/bitmark-inc/autonomy-api/utils"
 )
 
 func (s *Server) getSymptoms(c *gin.Context) {
@@ -52,6 +55,21 @@ func (s *Server) reportSymptoms(c *gin.Context) {
 		c.Error(err)
 		return
 	}
+	accts, err := s.mongoStore.NearestDistance(consts.CORHORT_DISTANCE_RANGE, *loc)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	utils.TriggerAccountUpdate(*s.cadenceClient, ctx, accts)
+
+	pois, err := s.mongoStore.NearestPOI(consts.CORHORT_DISTANCE_RANGE, *loc)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	utils.TriggerPOIUpdate(*s.cadenceClient, ctx, pois)
 	c.JSON(http.StatusOK, gin.H{"result": "OK"})
 
 	return
