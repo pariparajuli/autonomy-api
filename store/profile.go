a61f14494e31e39ac624061688a9799194671de0
pieceofr <komimi.p@gmail.com>
2020-04-20 19:48:22 +0800

    Add cadence functions for goodBehavior

--- store/profile.go
+++ store/profile.go
@@ -28,3 +28,22 @@ func (m *mongoDB) ProfileMetric(accountNumber string) (*schema.Metric, error) {
 	// TODO: update account metric when account created
 	return &p.Metric, nil
 }
+
+func (m *mongoDB) ProfileByAcctNumber(accountNumber string) (*schema.Profile, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	c := m.client.Database(m.database).Collection(schema.ProfileCollection)
+	query := bson.M{
+		"account_number": bson.M{
+			"$eq": accountNumber,
+		},
+	}
+
+	var p schema.Profile
+	err := c.FindOne(ctx, query).Decode(&p)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
