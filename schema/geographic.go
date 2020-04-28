2d82d6e72498b72d354af202f2375eca496d6861
Linzy Hu <linzy.hu@gmail.com>
2020-04-28 13:20:42 +0800

    add customized MarshalJSON for Geographic

--- schema/geographic.go
+++ schema/geographic.go
@@ -1,11 +1,25 @@
 package schema
 
+import (
+	"encoding/json"
+)
+
 const (
 	GeographicCollection = "geographic"
 )
 
 type Geographic struct {
-	AccountNumber string   `json:"-" bson:"account_number"`
-	Location      Location `json:"location" bson:"location"`
-	Timestamp     int64    `json:"timestamp" bson:"ts"`
+	AccountNumber string  `bson:"account_number"`
+	Location      GeoJSON `bson:"location"`
+	Timestamp     int64   `bson:"ts"`
+}
+
+func (g *Geographic) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Location  Location `json:"location"`
+		Timestamp int64    `json:"timestamp"`
+	}{
+		Location:  Location{Longitude: g.Location.Coordinates[0], Latitude: g.Location.Coordinates[1]},
+		Timestamp: g.Timestamp,
+	})
 }
