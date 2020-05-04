4f6e81e370b1c42eb42996200c7c6eba370c4f56
Aaron Chang <xesuschang@gmail.com>
2020-05-04 10:30:49 +0800

    add score confirm function

---
+++ score/confirm.go
@@ -0,0 +1,21 @@
+package score
+
+import "github.com/bitmark-inc/autonomy-api/schema"
+
+const (
+	confirmCriteria = 10
+)
+
+func ConfirmScore(metric *schema.Metric) {
+	details := &metric.Details.Confirm
+	delta := details.Today - details.Yesterday
+
+	// equal or more than that criteria get 0 score
+	if delta >= confirmCriteria {
+		details.Score = 0
+		return
+	}
+
+	// in between 0 - 10 people, each increased confirm case deduct 5 points
+	details.Score = 100 - 5*delta
+}
