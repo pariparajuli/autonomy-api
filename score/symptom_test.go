6175f5fe81397a6134365a96acffc57c945be219
Aaron Chang <xesuschang@gmail.com>
2020-05-05 11:46:40 +0800

    add test for SymptomScore make sure double run same result

--- score/symptom_test.go
+++ score/symptom_test.go
@@ -3,49 +3,52 @@ package score_test
 import (
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/bitmark-inc/autonomy-api/schema"
 	"github.com/bitmark-inc/autonomy-api/score"
 )
 
-func TestSymptomScore(t *testing.T) {
-	ctl := gomock.NewController(t)
-	defer ctl.Finish()
-
-	users := float64(3)
-	count := map[schema.SymptomType]int{
-		schema.Fever:   1,
-		schema.Face:    1,
-		schema.Chest:   1,
-		schema.Throat:  1,
-		schema.Nasal:   1,
-		schema.Cough:   1,
-		schema.Fatigue: 1,
-		schema.Breath:  1,
-	}
-	metric := schema.Metric{
-		Details: schema.Details{
-			Symptoms: schema.SymptomDetail{
-				TotalPeople: users,
-				Symptoms:    count,
-			},
+func TestSymptomScoreWhenDoTwice(t *testing.T) {
+	yesterday := score.NearestSymptomData{
+		UserCount:       1,
+		OfficialCount:   2,
+		CustomizedCount: 3,
+		WeightDistribution: map[schema.SymptomType]int{
+			schema.Fever:   1,
+			schema.Face:    1,
+			schema.Chest:   1,
+			schema.Throat:  1,
+			schema.Nasal:   1,
+			schema.Cough:   1,
+			schema.Fatigue: 1,
+			schema.Breath:  1,
 		},
 	}
 
-	score.SymptomScore(schema.DefaultSymptomWeights, &metric, nil)
-
-	var expectedTotal float64
-	var defaultMaxWeight float64
-	for k, v := range count {
-		expectedTotal += float64(v) * schema.DefaultSymptomWeights[k]
-		defaultMaxWeight += schema.DefaultSymptomWeights[k]
+	today := score.NearestSymptomData{
+		UserCount:       2,
+		OfficialCount:   3,
+		CustomizedCount: 4,
+		WeightDistribution: map[schema.SymptomType]int{
+			schema.Fever:   2,
+			schema.Face:    2,
+			schema.Chest:   2,
+			schema.Throat:  3,
+			schema.Nasal:   3,
+			schema.Cough:   3,
+			schema.Fatigue: 4,
+			schema.Breath:  5,
+		},
 	}
 
-	assert.Equal(t, expectedTotal, metric.Details.Symptoms.SymptomTotal, "wrong symptom total")
-	assert.Equal(t, users, metric.Details.Symptoms.TotalPeople, "wrong user")
-	assert.Equal(t, defaultMaxWeight, metric.Details.Symptoms.MaxScorePerPerson, "wrong max per person")
+	y1, y2, y3, y4, y5, y6 := score.SymptomScore(schema.DefaultSymptomWeights, today, yesterday)
+	t1, t2, t3, t4, t5, t6 := score.SymptomScore(schema.DefaultSymptomWeights, today, yesterday)
 
-	assert.Equal(t, 100-100*(expectedTotal/(users*defaultMaxWeight)), metric.Details.Symptoms.Score, "wrong score")
+	assert.Equal(t, t1, y1, "wrong symptom score")
+	assert.Equal(t, t2, y2, "wrong total weight")
+	assert.Equal(t, t3, y3, "wrong max score per person")
+	assert.Equal(t, t4, y4, "wrong delta in percent")
+	assert.Equal(t, t5, y5, "wrong official count")
+	assert.Equal(t, t6, y6, "wrong customized count")
 }
