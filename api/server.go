8616ad22d14b50f6fff9ca9fe055dc2a32c23f0b
Jim Yeh <jim@bitmark.com>
2020-05-14 16:14:56 +0800

    Use `PUT` for here api instead of `GET` (#47)

--- api/server.go
+++ api/server.go
@@ -147,7 +147,7 @@ func (s *Server) setupRouter() *gin.Engine {
 		accountRoute.PATCH("/me", s.accountUpdateMetadata)
 		accountRoute.DELETE("/me", s.accountDelete)
 
-		accountRoute.GET("/me/here", s.accountHere)
+		accountRoute.PUT("/me/here", s.accountHere)
 		accountRoute.GET("/me/profile_formula", s.getProfileFormula)
 		accountRoute.PUT("/me/profile_formula", s.updateProfileFormula)
 		accountRoute.DELETE("/me/profile_formula", s.resetProfileFormula)
