1407186165080f74c54000e1234249b002969479
pieceofr <komimi.p@gmail.com>
2020-05-15 15:56:15 +0800

    remove redudant log

--- store/confirmCDS.go
+++ store/confirmCDS.go
@@ -60,7 +60,6 @@ func (m *mongoDB) ReplaceCDS(result []schema.CDSData, country string) error {
 		return nil
 	}
 
-	log.WithFields(log.Fields{"prefix": mongoLogPrefix, "report_ts": result[0].ReportTime}).Debug("no record to update")
 	for _, v := range result {
 		filter := bson.M{"name": v.Name, "report_ts": v.ReportTime}
 		replacement := bson.M{
